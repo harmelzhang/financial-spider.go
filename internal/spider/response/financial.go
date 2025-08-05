f40733041504b5b2876cfb11dbe90bb9c6d6afd0
zhangpeng <harmel@163.com>
2025-08-05 15:45:58 +0800

    新增API响应对象实体

--- internal/spider/response/financial.go
+++ internal/spider/response/financial.go
@@ -1,1 +1,19 @@
 package response
+
+// 报告日期
+type ReportDate struct {
+	// 日期
+	Date string `json:"REPORT_DATE"`
+	// 日期类型
+	Type string `json:"REPORT_TYPE"`
+}
+
+// StockReportDateResult 报告期
+type StockReportDateResult struct {
+	// 总条数
+	Count int `json:"count"`
+	// 页码
+	Pages int `json:"pages"`
+	// 数据
+	Data []ReportDate `json:"data"` // 只有一个
+}
--- internal/spider/response/stock.go
+++ internal/spider/response/stock.go
@@ -1,10 +1,36 @@
 package response
 
 // 股票基本信息
-type StockBaseInfo struct{}
+type StockBaseInfo struct {
+	// 名称
+	Name string `json:"SECURITY_NAME_ABBR"`
+	// 曾用名称
+	BeforeName string `json:"FORMERNAME"`
+	// 公司名称
+	CompanyName string `json:"ORG_NAME"`
+	// 公司简介
+	CompanyProfile string `json:"ORG_PROFILE"`
+	// 地域
+	Region string `json:"PROVINCE"`
+	// 办公地址
+	Address string `json:"ADDRESS"`
+	// 公司网站
+	Website string `json:"ORG_WEB"`
+	// 经营范围
+	BusinessScope string `json:"BUSINESS_SCOPE"`
+	// 会计师事务所
+	AccountingFirm string `json:"ACCOUNTFIRM_NAME"`
+	// 律师事务所
+	LawFirm string `json:"LAW_FIRM"`
+}
 
 // 股票发行相关信息
-type StockListingInfo struct{}
+type StockListingInfo struct {
+	// 成立日期
+	CreateDate string `json:"FOUND_DATE"`
+	// 上市日期
+	ListingDate string `json:"LISTING_DATE"`
+}
 
 // 股票基本信息响应
 type StockBaseInfoResult struct {
@@ -12,6 +38,36 @@ type StockBaseInfoResult struct {
 	BaseInfo []StockBaseInfo `json:"jbzl"`
 	// 发行相关
 	ListingInfo []StockListingInfo `json:"fxxg"`
+}
+
+// -----
+
+// 主营业务
+type MainBusiness struct {
+	// 主营业务
+	Info string `json:"MAIN_BUSINESS"`
+}
+
+// 主营业务数据
+type MainBusinessData struct {
+	// 总条数
+	Count int `json:"count"`
+	// 页码
+	Pages int `json:"pages"`
+	// 数据
+	Data []MainBusiness `json:"data"` // 只有一个
+}
+
+// 主营业务响应
+type MainBusinessResult struct {
+	// 状态码
+	Code int `json:"code"`
+	// 消息
+	Message string `json:"message"`
+	// 是否成功
+	Success bool `json:"success"`
+	// 主营业务数据
+	Result MainBusinessData `json:"result"`
 }
 
 // -----
